3dfa0c675a156e06091676fc7fa8af3aad836151
katya0112 <hdzkatya07@gmail.com>
2022-08-29 09:14:12 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/salariofinal/SalarioFinal.java
@@ -0,0 +1,20 @@
+public class SalarioFinal {
+        
+        public static void main(String[] args) {
+        //actualizar datos numéricos:
+int salary = 1000;
+salary = salary + 200;
+System.out.println(salary); //1200
+
+salary += 300;
+System.out.println(salary); //1500
+
+salary = salary+(30*2)-50-40;
+System.out.println(salary); //1470
+
+
+
+
+        }
+}
+        
